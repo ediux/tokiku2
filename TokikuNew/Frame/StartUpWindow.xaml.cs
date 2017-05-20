b9d6ad860da85dcb2c8c67cab1ed31d906ecb0ba
ediux <ediux@msn.com>
2017-05-20 09:13:12 +0800

    加入啟動登入視窗

---
+++ TokikuNew/Frame/StartUpWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Tokiku.ViewModels;
+
+namespace TokikuNew.Frame
+{
+    /// <summary>
+    /// StartUpWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class StartUpWindow : Window
+    {
+        public static readonly DependencyProperty ModelProperty = DependencyProperty.Register("Model", typeof(LoginViewModel), typeof(StartUpWindow), new PropertyMetadata(default(LoginViewModel)));
+
+        public LoginViewModel Model
+        {
+            get { return GetValue(ModelProperty) as LoginViewModel; }
+            set { SetValue(ModelProperty, value); }
+        }
+
+        public StartUpWindow()
+        {
+            InitializeComponent();
+            Model = new LoginViewModel();
+            this.DataContext = Model;
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainwin = new MainWindow();
+            mainwin.Model = new MainViewModel();
+            mainwin.Model.LoginedUser = new Tokiku.Entity.Users() {
+                UserName=Model.UserName
+            };
+            mainwin.DataContext = mainwin.Model;
+            mainwin.Closed += Mainwin_Closed;
+            mainwin.Show();            
+            this.Hide();
+        }
+
+        private void Mainwin_Closed(object sender, EventArgs e)
+        {
+            this.Show();
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
