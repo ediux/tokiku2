94866a934c2ea2cb5647e059fc271b129ea76fc4
瑞欽 葉 <JuiChin.Yeh@cenoq.com>
2017-06-26 14:34:10 +0800

    變更專案合約管理

---
+++ Tokiku.ViewModels/專案合約管理ViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Tokiku.ViewModels
+{
+    public class 專案合約管理ViewModel : BaseViewModel
+    {
+        // 專案代碼
+        public string Code
+        {
+            get { return (string)GetValue(CodeProperty); }
+            set { SetValue(CodeProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Code.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CodeProperty =
+            DependencyProperty.Register("Code", typeof(string), typeof(專案合約管理ViewModel), new PropertyMetadata(string.Empty));
+
+        // 專案名稱
+        public string Name
+        {
+            get { return (string)GetValue(NameProperty); }
+            set { SetValue(NameProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Name.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty NameProperty =
+            DependencyProperty.Register("Name", typeof(string), typeof(專案合約管理ViewModel), new PropertyMetadata(string.Empty));
+
+
+    }
+}
