020ebe3884fb6c40d9073e4fda9bf9f4b973d88c
瑞欽 葉 <JuiChin.Yeh@cenoq.com>
2017-07-11 11:03:05 +0800

    本票管理, 合約管理

--- Tokiku.ViewModels/Projects/ContractManagementViewModel.cs
+++ Tokiku.ViewModels/Projects/ContractManagementViewModel.cs
@@ -3,15 +3,59 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tokiku.Controllers;
+using Tokiku.Entity;
 
 namespace Tokiku.ViewModels
 {
     public class ContractManagementViewModelCollection : BaseViewModelCollection<ContractManagementViewModel>
     {
+        public ContractManagementViewModelCollection()
+        {
+            HasError = false;
+        }
+
+        public ContractManagementViewModelCollection(IEnumerable<ContractManagementViewModel> source) : base(source)
+        {
+
+
+        }
+
+        public override void Query()
+        {
+            ContractManagementController ctrl = new ContractManagementController();
+            ExecuteResultEntity<ICollection<ContractManagementEntity>> ere = ctrl.QuerAll();
+            if (!ere.HasError)
+            {
+                ContractManagementViewModel vm = new ContractManagementViewModel();
+                foreach (var item in ere.Result)
+                {
+                    vm.SetModel(item);
+                    Add(vm);
+                }
+            }
+        }
 
     }
 
     public class ContractManagementViewModel : BaseViewModel
     {
+        public override void SetModel(dynamic entity)
+        {
+            try
+            {
+                if (entity is ContractManagementEntity)
+                {
+                    ContractManagementEntity data = (ContractManagementEntity)entity;
+                    BindingFromModel(data, this);
+                }
+            }
+            catch (Exception ex)
+            {
+                setErrortoModel(this, ex);
+                throw;
+            }
+        }
+
     }
 }
