f83acb17f53b6aedf659b0164d4a574f9cb79f3f
瑞欽 葉 <JuiChin.Yeh@cenoq.com>
2017-07-11 10:06:03 +0800

    人員管理, 角色管理

---
+++ Tokiku.ViewModels/SystemMembersViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tokiku.Controllers;
+using Tokiku.Entity;
+using Tokiku.Entity.ViewTables;
+
+namespace Tokiku.ViewModels
+{
+    public class SystemMembersViewModelCollection : BaseViewModelCollection<SystemMembersViewModel>
+    {
+        public SystemMembersViewModelCollection()
+        {
+            HasError = false;
+        }
+
+        public SystemMembersViewModelCollection(IEnumerable<SystemMembersViewModel> source) : base(source)
+        {
+
+        }
+
+        public override void Query()
+        {
+            SystemMembersController ctrl = new SystemMembersController();
+            ExecuteResultEntity<ICollection<SystemMembersEntity>> ere = ctrl.QuerAll();
+            if (!ere.HasError)
+            {
+                SystemMembersViewModel vm = new SystemMembersViewModel();
+                foreach (var item in ere.Result)
+                {
+                    vm.SetModel(item);
+                    Add(vm);
+                }
+            }
+        }
+
+    }
+
+    public class SystemMembersViewModel : BaseViewModel
+    {
+        public override void SetModel(dynamic entity)
+        {
+            try
+            {
+                if (entity is SystemMembersEntity)
+                {
+                    SystemMembersEntity data = (SystemMembersEntity)entity;
+                    BindingFromModel(data, this);
+                }
+            }
+            catch (Exception ex)
+            {
+                setErrortoModel(this, ex);
+                throw;
+            }
+        }
+
+    }
+}
---
+++ Tokiku.ViewModels/SystemRolesViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tokiku.Controllers;
+using Tokiku.Entity;
+
+namespace Tokiku.ViewModels
+{
+    public class SystemRolesViewModelCollection : BaseViewModelCollection<SystemRolesViewModel>
+    {
+        public SystemRolesViewModelCollection()
+        {
+            HasError = false;
+        }
+
+        public SystemRolesViewModelCollection(IEnumerable<SystemRolesViewModel> source) : base(source)
+        {
+
+        }
+
+        public override void Query()
+        {
+            SystemRolesController ctrl = new SystemRolesController();
+            ExecuteResultEntity<ICollection<SystemRolesEntity>> ere = ctrl.QuerAll();
+            if (!ere.HasError)
+            {
+                SystemRolesViewModel vm = new SystemRolesViewModel();
+                foreach (var item in ere.Result)
+                {
+                    vm.SetModel(item);
+                    Add(vm);
+                }
+            }
+        }
+
+    }
+
+    public class SystemRolesViewModel : BaseViewModel
+    {
+        public override void SetModel(dynamic entity)
+        {
+            try
+            {
+                if (entity is SystemRolesEntity)
+                {
+                    SystemRolesEntity data = (SystemRolesEntity)entity;
+                    BindingFromModel(data, this);
+                }
+            }
+            catch (Exception ex)
+            {
+                setErrortoModel(this, ex);
+                throw;
+            }
+        }
+
+    }
+}
