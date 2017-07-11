39ce474b6b5614f98ef9b532b04472db184b1b12
瑞欽 葉 <JuiChin.Yeh@cenoq.com>
2017-07-11 18:16:09 +0800

    新增 訂製單管控表

---
+++ Tokiku.Controllers/OrderControlTableController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tokiku.Entity;
+
+namespace Tokiku.Controllers
+{
+    public class OrderControlTableController : BaseController
+    {
+        private string sql;
+
+        public ExecuteResultEntity<ICollection<OrderControlTableEntity>> QuerAll()
+        {
+            sql = " select TokikuId, ManufacturersId, Material, UnitWeight, OrderLength, " +
+                         " RequiredQuantity, SparePartsQuantity, PlaceAnOrderQuantity, Note " +
+                    " from TABL1 ";
+
+            ExecuteResultEntity<ICollection<OrderControlTableEntity>> rtn;
+
+            try
+            {
+                using (var ManufacturersRepository = RepositoryHelper.GetManufacturersRepository())
+                {
+                    var queryresult = ManufacturersRepository.UnitOfWork.Context.Database.SqlQuery<OrderControlTableEntity>(sql);
+
+                    rtn = ExecuteResultEntity<ICollection<OrderControlTableEntity>>.CreateResultEntity(
+                        new Collection<OrderControlTableEntity>(queryresult.ToList()));
+
+                    return rtn;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                rtn = ExecuteResultEntity<ICollection<OrderControlTableEntity>>.CreateErrorResultEntity(ex);
+                return rtn;
+            }
+        }
+    }
+}
---
+++ TokikuNew/Views/OrderControlTableView.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Tokiku.ViewModels;
+
+namespace TokikuNew.Views
+{
+    /// <summary>
+    /// OrderControlTableView.xaml 的互動邏輯
+    /// </summary>
+    public partial class OrderControlTableView : UserControl
+    {
+        public OrderControlTableView()
+        {
+            InitializeComponent();
+        }
+
+        private void OrderControlTableView_Loaded(object sender, RoutedEventArgs e)
+        {
+            OrderControlTableViewModelCollection ctrl = new OrderControlTableViewModelCollection();
+            CheckGrid.DataContext = ctrl;
+            ctrl.Query();
+        }
+    }
+}
