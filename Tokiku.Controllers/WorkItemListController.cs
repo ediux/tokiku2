f3d0ad671ce2051d79ded371a62c30bc9d29ad90
瑞欽 葉 <JuiChin.Yeh@cenoq.com>
2017-07-20 15:25:06 +0800

    專案合約工程項目

---
+++ Tokiku.Controllers/WorkItemListController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tokiku.Entity;
+
+namespace Tokiku.Controllers
+{
+    public class WorkItemListController : BaseController
+    {
+        public ExecuteResultEntity<ICollection<Engineering>> QuerAll()
+        {
+            try {
+                var repo = RepositoryHelper.GetEngineeringRepository();
+                return ExecuteResultEntity<ICollection<Engineering>>.CreateResultEntity(
+                    new Collection<Engineering>(repo.All().ToList()));
+            }catch (Exception ex) {
+                return ExecuteResultEntity<ICollection<Engineering>>.CreateErrorResultEntity(ex);
+            }
+        }
+    }
+}
---
+++ Tokiku.ViewModels/WorkItemListViewModels.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tokiku.Entity;
+
+namespace Tokiku.ViewModels
+{
+    public class WorkItemListCollection : BaseViewModelCollection<WorkItemListViewModels>
+    {
+        public WorkItemListCollection()
+        {
+        }
+
+        public WorkItemListCollection(IEnumerable<WorkItemListViewModels> source) : base(source)
+        {
+        }
+
+        public static WorkItemListCollection Query()
+        {
+            return Query<WorkItemListCollection, Engineering>("Engineering", "QueryAll");
+        }
+    }
+
+    public class WorkItemListViewModels : BaseViewModelWithPOCOClass<Engineering>
+    {
+        public WorkItemListViewModels(Engineering entity) : base(entity)
+        {
+        }
+
+        // 合約編號
+        public string ContractNumber
+        {
+            get { return CopyofPOCOInstance.ProjectContract.ContractNumber; }
+            set { CopyofPOCOInstance.ProjectContract.ContractNumber = value; RaisePropertyChanged("ContractNumber"); }
+        }
+
+        // 工程名稱
+        public string Name
+        {
+            get { return CopyofPOCOInstance.Name; }
+            set { CopyofPOCOInstance.Name = value; RaisePropertyChanged("Name"); }
+        }
+
+        // 單位
+        public string Unit
+        {
+            get { return CopyofPOCOInstance.Unit; }
+            set { CopyofPOCOInstance.Unit = value; RaisePropertyChanged("Unit"); }
+        }
+
+    }
+}
