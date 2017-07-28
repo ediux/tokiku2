c7124223591d04a9d26eadeb60b28cdf5f6e33b6
瑞欽 葉 <JuiChin.Yeh@cenoq.com>
2017-07-28 17:45:01 +0800

    出貨單

--- Tokiku.ViewModels/ControlTables/ShippingDetailsViewModel.cs
+++ Tokiku.ViewModels/ControlTables/ShippingDetailsViewModel.cs
@@ -29,5 +29,103 @@ namespace Tokiku.ViewModels
         {
         }
 
+        // 領料單單號
+        public string PickListNumber
+        {
+            get { return CopyofPOCOInstance.PickListNumber; }
+            set { CopyofPOCOInstance.PickListNumber = value; RaisePropertyChanged("PickListNumber"); }
+        }
+
+        // 批號
+        public string BatchNumber
+        {
+            get { return CopyofPOCOInstance.BatchNumber; }
+            set { CopyofPOCOInstance.BatchNumber = value; RaisePropertyChanged("BatchNumber"); }
+        }
+
+        // 東菊編號
+        public string RequiredDetailsCode
+        {
+            get { return CopyofPOCOInstance.RequiredDetailsCode; }
+            set { CopyofPOCOInstance.RequiredDetailsCode = value; RaisePropertyChanged("RequiredDetailsCode"); }
+        }
+
+        // 廠商編號
+        public string ManufacturersCode
+        {
+            get { return CopyofPOCOInstance.ManufacturersCode; }
+            set { CopyofPOCOInstance.ManufacturersCode = value; RaisePropertyChanged("ManufacturersCode"); }
+        }
+
+        // 廠商名稱
+        public string ManufacturersName
+        {
+            get { return CopyofPOCOInstance.ManufacturersName; }
+            set { CopyofPOCOInstance.ManufacturersName = value; RaisePropertyChanged("ManufacturersName"); }
+        }
+
+        // 材質
+        public string MaterialsName
+        {
+            get { return CopyofPOCOInstance.MaterialsName; }
+            set { CopyofPOCOInstance.MaterialsName = value; RaisePropertyChanged("MaterialsName"); }
+        }
+
+        // 單位重(kg/m)
+        public string UnitWeight
+        {
+            get { return CopyofPOCOInstance.UnitWeight; }
+            set { CopyofPOCOInstance.UnitWeight = value; RaisePropertyChanged("UnitWeight"); }
+        }
+
+        // 訂購長度(mm)
+        public string OrderLength
+        {
+            get { return CopyofPOCOInstance.OrderLength; }
+            set { CopyofPOCOInstance.OrderLength = value; RaisePropertyChanged("OrderLength"); }
+        }
+
+        // 下單數量
+        public string OrderQuantity
+        {
+            get { return CopyofPOCOInstance.OrderQuantity; }
+            set { CopyofPOCOInstance.OrderQuantity = value; RaisePropertyChanged("OrderQuantity"); }
+        }
+
+        // 出貨順序
+        public string ShippingOrder
+        {
+            get { return CopyofPOCOInstance.ShippingOrder; }
+            set { CopyofPOCOInstance.ShippingOrder = value; RaisePropertyChanged("ShippingOrder"); }
+        }
+
+        // 出貨重量
+        public string Weight
+        {
+            get { return CopyofPOCOInstance.Weight; }
+            set { CopyofPOCOInstance.Weight = value; RaisePropertyChanged("Weight"); }
+        }
+
+        // 缺貨數量
+        public string LackQuantity
+        {
+            get { return CopyofPOCOInstance.LackQuantity; }
+            set { CopyofPOCOInstance.LackQuantity = value; RaisePropertyChanged("LackQuantity"); }
+        }
+
+        // 出貨數量
+        public string ShippingQuantity
+        {
+            get { return CopyofPOCOInstance.ShippingQuantity; }
+            set { CopyofPOCOInstance.ShippingQuantity = value; RaisePropertyChanged("ShippingQuantity"); }
+        }
+
+        // 備註
+        public string Comment
+        {
+            get { return CopyofPOCOInstance.Comment; }
+            set { CopyofPOCOInstance.Comment = value; RaisePropertyChanged("Comment"); }
+        }
+
     }
 }
