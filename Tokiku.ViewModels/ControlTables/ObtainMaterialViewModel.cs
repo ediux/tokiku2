94cfe7165c0de55a09ea3cdc9e19bff23d7cc357
瑞欽 葉 <JuiChin.Yeh@cenoq.com>
2017-07-31 19:09:02 +0800

    領料單

--- Tokiku.ViewModels/ControlTables/ObtainMaterialViewModel.cs
+++ Tokiku.ViewModels/ControlTables/ObtainMaterialViewModel.cs
@@ -19,14 +19,64 @@ namespace Tokiku.ViewModels
 
         public static ObtainMaterialViewModelCollection Query()
         {
-            return Query<ObtainMaterialViewModelCollection, PickList>("ObtainMaterial", "QueryAll");
+            return Query<ObtainMaterialViewModelCollection, PickListDetails>("ObtainMaterial", "QueryAll");
         }
     }
 
-    public class ObtainMaterialViewModel : BaseViewModelWithPOCOClass<PickList>
+    public class ObtainMaterialViewModel : BaseViewModelWithPOCOClass<PickListDetails>
     {
-        public ObtainMaterialViewModel(PickList entity) : base(entity)
+        public ObtainMaterialViewModel(PickListDetails entity) : base(entity)
         {
         }
+
+        // 東菊編號
+        public string RequiredDetailsCode
+        {
+            get { return CopyofPOCOInstance.OrderDetails.RequiredDetails.Code; }
+            set { CopyofPOCOInstance.OrderDetails.RequiredDetails.Code = value; RaisePropertyChanged("RequiredDetailsCode"); }
+        }
+
+        // 廠商編號
+        public string FactoryNumber
+        {
+            get { return CopyofPOCOInstance.OrderDetails.RequiredDetails.FactoryNumber; }
+            set { CopyofPOCOInstance.OrderDetails.RequiredDetails.FactoryNumber = value; RaisePropertyChanged("FactoryNumber"); }
+        }
+
+        // 單位重
+        public decimal UnitWeight
+        {
+            get { return CopyofPOCOInstance.OrderDetails.RequiredDetails.UnitWeight; }
+            set { CopyofPOCOInstance.OrderDetails.RequiredDetails.UnitWeight = value; RaisePropertyChanged("UnitWeight"); }
+        }
+
+        // 訂購長度
+        public int? OrderLength
+        {
+            get { return CopyofPOCOInstance.OrderDetails.RequiredDetails.OrderLength; }
+            set { CopyofPOCOInstance.OrderDetails.RequiredDetails.OrderLength = value; RaisePropertyChanged("OrderLength"); }
+        }
+
+        // 領料數量
+        public int ShippingQuantity
+        {
+            get { return CopyofPOCOInstance.ShippingQuantity; }
+            set { CopyofPOCOInstance.ShippingQuantity = value; RaisePropertyChanged("ShippingQuantity"); }
+        }
+
+        // 庫存結餘數量
+        public decimal? InventoryStatus_QuantitySubtotal
+        {
+            get { return CopyofPOCOInstance.OrderDetails.ControlTableDetails.InventoryStatus_QuantitySubtotal; }
+            set { CopyofPOCOInstance.OrderDetails.ControlTableDetails.InventoryStatus_QuantitySubtotal = value; RaisePropertyChanged("InventoryStatus_QuantitySubtotal"); }
+        }
+
+        // 備註
+        public string Comment
+        {
+            get { return CopyofPOCOInstance.Comment; }
+            set { CopyofPOCOInstance.Comment = value; RaisePropertyChanged("Comment"); }
+        }
+
     }
 }
